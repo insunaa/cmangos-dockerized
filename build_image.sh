81f30ae63c9bbf8c00b95c3a2d3845acea509fdf
insunaa <insuna@no.capfr.fr>
2025-11-20 14:30:48 +0100

    Builder: additional tag with date, to allow rollbacks

--- build_image.sh
+++ build_image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source preamble.sh
 
-$ORCH build --build-arg AHBOT=$BUILD_PLAYERBOTS --build-arg PLAYERBOTS=$BUILD_PLAYERBOTS --build-arg CMANGOS_EXPANSION=$CMANGOS_EXPANSION -t cmangos-$CMANGOS_EXPANSION:latest .
+$ORCH build --build-arg AHBOT=$BUILD_PLAYERBOTS --build-arg PLAYERBOTS=$BUILD_PLAYERBOTS --build-arg CMANGOS_EXPANSION=$CMANGOS_EXPANSION -t cmangos-$CMANGOS_EXPANSION:latest -t cmangos-$CMANGOS_EXPANSION:$(date -I) .
 
 $ORCH rmi $($ORCH images --filter label=deleteme=true -q)
